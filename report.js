6a7b040a17e02edeae33de565f23f9d04ac9542a
suzakr <suzakr@seznam.cz>
2024-07-11 10:14:42 +0200

    Create report.js

---
+++ report.js
@@ -0,0 +1,58 @@
+#  Martin Fedorcak / Discord - Martin F.
+
+{"name": "Martin Fedorcak","date": "10-07-2024/12:03","winner": "Andoes","data":[{
+"type": "Andoes",
+"total_count": 42,
+"min_length": 42,
+"min_lower": 42,
+"min_upper": 42,
+"min_digit": 0,
+"min_special": 27,
+"valid_prefix": 42,
+"valid_suffix": 42,
+"score": 5
+},{
+"type": "Cairo",
+"total_count": 99,
+"min_length": 81,
+"min_lower": 97,
+"min_upper": 97,
+"min_digit": 73,
+"min_special": 62,
+"valid_prefix": 99,
+"valid_suffix": 94,
+"score": 1
+},{
+"type": "HernsPW",
+"total_count": 91,
+"min_length": 91,
+"min_lower": 91,
+"min_upper": 91,
+"min_digit": 85,
+"min_special": 71,
+"valid_prefix": 91,
+"valid_suffix": 90,
+"score": 4
+},{
+"type": "Orion1.15",
+"total_count": 85,
+"min_length": 85,
+"min_lower": 85,
+"min_upper": 84,
+"min_digit": 73,
+"min_special": 0,
+"valid_prefix": 84,
+"valid_suffix": 85,
+"score": 3
+},{
+"type": "PWmk",
+"total_count": 74,
+"min_length": 63,
+"min_lower": 74,
+"min_upper": 74,
+"min_digit": 0,
+"min_special": 55,
+"valid_prefix": 74,
+"valid_suffix": 72,
+"score": 3
+}]}
